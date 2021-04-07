25fface3714efc811513745431cde23c3d332a0d
emmanuellekamwa <emmanuellekamwa@gmail.com>
2021-04-07 11:47:45 +0100

    Create friend request button

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,10 @@
 module FriendshipsHelper
+    def ask_friendship_or(user, friend)
+        friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
+        return if !friendship.nil? || user == friend
+    
+        content_tag(:button, class: 'btn btn-info btn-lg btn-block') do
+          link_to 'Add Friend', friendships_path(user_id: user, friend_id: friend), method: :post, class: 'text-white'
+        end
+    end
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,20 @@
+module UsersHelper
+    def invite_status(invitee)
+      res = ''
+      return if current_user.id == invitee.id
+  
+      if current_user.pending_friends.include?(invitee)
+        res << link_to('Pending Request', '#')
+      elsif current_user.requested_friends.include?(invitee)
+        res << link_to('Accept', invite_path(user_id: invitee.id), method: :put)
+        res << ' | '
+        res << link_to('Reject', reject_path(user_id: invitee.id), method: :delete)
+      elsif current_user.friend?(invitee) || invitee.friend?(current_user)
+        res << link_to('Friends', '#')
+      else
+        res << link_to('Add Friend', invite_path(user_id: invitee.id), method: :post)
+      end
+  
+      res.html_safe
+    end
+end
